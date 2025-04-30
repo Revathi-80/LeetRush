c235fed2adb9c033dcee38b4e201ba247699f5cd
revathi <revathi@v-comply.com>
2025-04-30 22:31:49 +0530

    [add] register controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,16 +1,75 @@
+import bcrypt from "bcryptjs";
+import { db } from "../libs/db.js";
+import { UserRole } from "../generated/prisma/index.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
 
-}
+  if (!email || !password) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
+    });
+  }
 
-export const loginUser = async (req, res) => {
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-}
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
 
-export const getProfile = async (req, res) => {
-    
-}   
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-export const logoutUser = async (req, res) => {
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role: UserRole.USER,
+      },
+    });
 
-}
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error,
+      message: "Something went wrong in register user",
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {};
+
+export const getProfile = async (req, res) => {};
+
+export const logoutUser = async (req, res) => {};
